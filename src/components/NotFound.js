8535d23436e83e41d587b7d103ae3869614c8ea3
Jeongmin Byun <jmbyun91@gmail.com>
2018-09-18 16:25:12 +0900

    fix layout of not found page and build the app

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,16 +5,18 @@ export default class NotFound extends Component {
   render() {
     return (
       <div className="c-not-found">
-        <img
-          className="c-not-found__icon"
-          src="/public/images/confusedparrot.gif"
-          alt="confused parrot"
-        />
-        <div className="c-not-found__title">
-          404
-        </div>
-        <div className="c-not-found__body">
-          This is not the place you were looking for.
+        <div className="u-width-container">
+          <img
+            className="c-not-found__icon"
+            src="/public/images/confusedparrot.gif"
+            alt="confused parrot"
+          />
+          <div className="c-not-found__title">
+            404
+          </div>
+          <div className="c-not-found__body">
+            This is not the place you were looking for.
+          </div>
         </div>
       </div>
     );
